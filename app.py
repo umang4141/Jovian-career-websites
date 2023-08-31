157b78569c152761d556fb8d33b01bd4f8053ca8
umang.singh <umang.singh@magicedtech.com>
2023-08-31 17:11:27 +0530

    New Changes

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 def hello_world():
     return "Hello,world"
 
+@app.route("/products")
+def welcome():
+    return "welcome to the API"
+
 if __name__ =="__main__":
     # print("I am inside the if now")
     app.run(host='0.0.0.0',debug=True)
-
